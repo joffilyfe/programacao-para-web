d026fe11852948026aee63e64ddfbe2959aa9d76
Joffily Ferreira <joffily@outlook.com>
2016-10-31 16:30:30 -0200

    Adicionado bat manusear o banco de dados com interface gráfica

--- hsqldb/bin/memoriam.bat
+++ hsqldb/bin/memoriam.bat
@@ -1,1 +1,1 @@
-java -cp .\lib\hsqldb.jar org.hsqldb.server.Server --database.0 file:..\databases\memoriam --dbname.0 memoriam
+java -cp ..\lib\hsqldb.jar org.hsqldb.server.Server --database.0 file:..\..\databases\memoriam --dbname.0 memoriam
