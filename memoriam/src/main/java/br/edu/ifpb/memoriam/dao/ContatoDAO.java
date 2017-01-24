cc79a885c03a6416fb3e030810fd432bf23d9fc0
Joffily Ferreira <joffily@outlook.com>
2017-01-24 00:04:49 -0300

    Feature: Procurando contatos de um usuário

--- memoriam/src/main/java/br/edu/ifpb/memoriam/dao/ContatoDAO.java
+++ memoriam/src/main/java/br/edu/ifpb/memoriam/dao/ContatoDAO.java
@@ -1,11 +1,15 @@
 package br.edu.ifpb.memoriam.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import br.edu.ifpb.memoriam.entity.Contato;
+import br.edu.ifpb.memoriam.entity.Usuario;
 
 public class ContatoDAO extends GenericDAO<Contato, Integer> {
-	
+
 	public ContatoDAO() {
 		this(PersistenceUtil.getCurrentEntityManager());
 	}
@@ -14,5 +18,19 @@ public class ContatoDAO extends GenericDAO<Contato, Integer> {
 		super(em);
 	}
 
-	
+	public List<Contato> findAllFromUser(Usuario usuario) {
+		Query q = this.getEntityManager().createQuery("SELECT c FROM Contato c WHERE c.usuario = :user");
+		q.setParameter("user", usuario);
+
+		if (q.getResultList().isEmpty()) {
+			return null;
+		}
+
+		@SuppressWarnings("unchecked")
+		List<Contato> l = q.getResultList();
+
+		return l;
+	}
+
+
 }
